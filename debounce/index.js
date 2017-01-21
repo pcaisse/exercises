6b0e46c2a86996d7e849c3b2b61080949b97eeb4
Peter Caisse <pcaisse@gmail.com>
2017-01-21 01:18:42 -0500

    Rework function to delay the function call each time it's called instead of ignoring subsequent calls

--- debounce/index.js
+++ debounce/index.js
@@ -1,13 +1,18 @@
+/**
+ * Wait a certain number of ms before executing function, resetting timer for each subsequent call.
+ */
 function debounce(func, ms) {
-    var isWaitingToExecute = false;
+    var currCount = 0;
     return function() {
-        if (!isWaitingToExecute) {
-            setTimeout(function() {
-                func.apply(this, arguments);
-                isWaitingToExecute = false;
-            }.bind(this), ms);  // bind context explicitly
-            isWaitingToExecute = true;
-        }
+        var args = arguments;  // save off arguments so they can be passed to function call
+        currCount++;
+        setTimeout(function(count) {
+            // Only call function if another subsequent call was not made in the interim
+            if (count === currCount) {
+                func.apply(this, args);
+                currCount = 0;
+            }
+        }.bind(this, currCount), ms);  // bind context explicitly
     };
 }
 

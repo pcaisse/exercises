a0f59522c6e9fd8c3bb50816116b7476322a751c
Peter Caisse <pcaisse@gmail.com>
2017-01-22 14:48:15 -0500

    Simplify implementation leveraging Object.assign

--- update/index.js
+++ update/index.js
@@ -1,62 +1,62 @@
 function isObject(obj) {
     return obj === Object(obj);
 }
+function safeHasOwnProperty(obj, key) {
+    // Note: this will still work even if obj.hasOwnProperty has been overridden
+    return Object.prototype.hasOwnProperty.call(obj, key);
+}
+/**
+ * Recursively search through object for command
+ */
 function findCommandInKeys(obj) {
-    var keys = Object.keys(obj);
-    for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
-        console.log('key', key);
-        var value = obj[key];
-        if (['$merge', '$set', '$push', '$unshift', '$merge', '$apply', '$splice'].includes(key)) {
-            console.log('found key!', key);
-            return key;
-        } else if (isObject(value)) {
-            console.log('must go deeper with value ', value);
-            return findCommandInKeys(value);
-        } else {
-            throw new Error('Command not found in object');
+    for (var key in obj) {
+        console.log('obj', obj);
+        if (safeHasOwnProperty(obj, key)) {
+            console.log('key', key);
+            var value = obj[key];
+            if (['$merge', '$set', '$push', '$unshift', '$merge', '$apply', '$splice'].includes(key)) {
+                console.log('found key!', key);
+                return key;
+            } else if (isObject(value)) {
+                console.log('must go deeper with value ', value);
+                return findCommandInKeys(value);
+            } else {
+                throw new Error('Command not found in object');
+            }
         }
     }
 }
 /**
- * Set value in objA using matching value found in objB like {'$set': <value>}
+ * Mutate value in object using value found within its own nested object like {'$set': <value>}
  */
-function setValueInObj(objA, objB) {
-    console.log('objA', objA);
-    console.log('objB', objB);
-    if (objB.hasOwnProperty('$set')) {
-        return objB['$set'];
-    }
-    var keysA = Object.keys(objA);
-    console.log('keysA', keysA);
-    var key, value, newValue;
-    // iterate over all keys in objA
-    for (var i = 0; i < keysA.length; i++) {
-        key = keysA[i];
-        value = objA[key];
-        console.log('key', key, 'value', value);
-        if (isObject(value)) {
-            // value is an object itself so we make a recursive call on current value
-            // and the potentially matching value from objB if it exists
-            // and set the key's value to that new value
-            console.log('isObject(value)', isObject(value));
-            newValue = setValueInObj(value, objB[key]);
-            console.log('newValue', newValue);
-            objA[key] = newValue;
-            return objA;
-        } else if (objA.hasOwnProperty(key) && objB.hasOwnProperty(key)) {
-            // fix up our non-obj key values
-            console.log('key is shared', key);
-            console.log('objB[key]', objB[key]);
-            if (isObject(objB[key]) && objB[key].hasOwnProperty('$set')) {
-                objA[key] = objB[key]['$set'];
+function setValueInObj(obj) {
+    console.log('obj', obj);
+    for (var key in obj) {
+        if (safeHasOwnProperty(obj, key)) {
+            var value = obj[key];
+            console.log('key', key, 'value', value);
+            if (key === '$set') {
+                // replace $set object with just its value
+                console.log('replace set');
+                return value;
+            } else if (isObject(value)) {
+                // call on value which is itself an object
+                // and update with new value
+                var newValue = setValueInObj(value);
+                obj[key] = newValue;
+                return obj;
             }
-            return objA;
         }
     }
 }
 var FUNCS = {
-    $set: setValueInObj,
+    $set: function(a, b) {
+        if (safeHasOwnProperty(b, '$set')) {
+            // replace a with value of b
+            return b['$set'];
+        }
+        return Object.assign({}, a, setValueInObj(b));
+    },
     $push: function(a, b) {
         return a.concat(b['$push']);
     },
